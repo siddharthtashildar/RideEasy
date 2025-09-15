8c24058b8234c58f893496965e629c700e995701
Sid <siddharthtashildar17@gmail.com>
2025-09-15 12:53:40 +0530

    more vehicles deez nutss

---
+++ models/sedan.js
@@ -0,0 +1,18 @@
+import Vehicle from './Vehicle';
+
+
+export default class sedan extends Vehicle {
+    constructor(make, model, plate) {
+        super(make, model);
+        this._plate = plate;
+    }
+    get plate() {
+        return this._plate;
+    }
+    set plate(newPlate) {
+        this._plate = newPlate;
+    }
+    getInfo() {
+        return `${super.getInfo()} (Plate: ${this._plate})`;
+    }
+}
